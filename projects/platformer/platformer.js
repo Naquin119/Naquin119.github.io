18d1aff4684d2e4bfe7ddcb7f2f357ea5d1d0db4
Naquin119 <157381510+Naquin119@users.noreply.github.com>
2024-02-21 13:33:31 +0000

    fixed code error

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -42,11 +42,9 @@ $(function () {
     // TODO 1
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
-    // example usage: createPlatform(x,y,width,height)
+    // example usage: createPlatform(x,y,width,height)   
 
-
-
-    
+    createPlatform(500, 300, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
